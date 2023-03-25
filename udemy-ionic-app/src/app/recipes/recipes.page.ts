4049d9884d0ca5b2292eb3602e000cb9dc0b927e
Bravo1B9 <brofe93@gmail.com>
2023-03-25 11:59:45 +0000

    Create and setup recipes service

--- udemy-ionic-app/src/app/recipes/recipes.page.ts
+++ udemy-ionic-app/src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,31 +8,12 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Schnitzel',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
-      ingredients: ['French fries', 'Vegan meat', 'Salad']
-    },
-    {
-      id: 'r2',
-      title: 'Burger',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/375px-Hamburger_%28black_bg%29.jpg',
-      ingredients: ['French fries', 'Vegan patty', 'tomato']
-    },
-    {
-      id: 'r3',
-      title: 'Spagghetti',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg/375px-Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg',
-      ingredients: ['French fries', 'Vegan meat balls', 'spaghetti']
-    },
-  ];
 
-  constructor() {}
+  recipes: Recipe[] = [];
 
-  ngOnInit() {}
+  constructor(private recipeService: RecipesService) {}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getAllRecipes();
+  }
 }
--- udemy-ionic-app/src/app/recipes/recipes.service.ts
+++ udemy-ionic-app/src/app/recipes/recipes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,39 @@ import { Injectable } from '@angular/core';
 export class RecipesService {
 
   constructor() { }
+
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['French fries', 'Vegan meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Burger',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/375px-Hamburger_%28black_bg%29.jpg',
+      ingredients: ['French fries', 'Vegan patty', 'tomato']
+    },
+    {
+      id: 'r3',
+      title: 'Spagghetti',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg/375px-Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg',
+      ingredients: ['French fries', 'Vegan meat balls', 'spaghetti']
+    },
+  ];
+
+  getAllRecipes() {
+    return [...this.recipes];
+  };
+
+  getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })};
+  }
+
 }
